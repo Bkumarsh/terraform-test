d443e25aa7763ce3f175ed37bfc35869cfa98024
Babloo <master@gmail.com>
2025-05-09 22:01:34 +0530

    modify in main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "azurerm_resource_group" "azure_rg" {
   location = "East US"
 }
 
-# resource "azurerm_storage_account" "azure_sgc" {
-#   name                     = "dhondhustorageaccount"
-#   resource_group_name      = azurerm_resource_group.azure_rg.name
-#   location                 = azurerm_resource_group.azure_rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "azure_sgc" {
+  name                     = "dhondhustorageaccount"
+  resource_group_name      = azurerm_resource_group.azure_rg.name
+  location                 = azurerm_resource_group.azure_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
